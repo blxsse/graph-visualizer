d1f8e9899fc932d8653b14f4a75dc79cea11df12
Blisse K <76980525+blisseko@users.noreply.github.com>
2022-06-09 17:00:58 -0400

    Refactoring

--- script.ts
+++ script.ts
@@ -1,11 +1,23 @@
-const NODE_SIZE = 10;
-const CANVAS_SIZE = 100; // needs to be changed in the index.html file as well
+const NODE_SIZE = 30;
+const CANVAS_SIZE = 900; // needs to be changed in the index.html file as well
 const NUMBER_NODES_PER_SIDE = CANVAS_SIZE / NODE_SIZE;
 
 const canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement ?? null;
 if (canvas === undefined || canvas === null) {
     throw new Error;
 }
+
+/**
+ * Draws a rectangle with dimensions `width` x `height` and upper left corner coordinate (x, y).
+ * 
+ * @param canvas 
+ * @param x upper left corner's x-coordinate
+ * @param y upper left corner's y-coordinate
+ * @param width 
+ * @param height 
+ * @param thickness border thickness
+ * @param color border color
+ */
 function sketchBox(canvas: HTMLCanvasElement, x: number, y: number, width: number, height: number, thickness: number, color: string): void {
     const context = canvas.getContext("2d");
     if (context === undefined || context === null) {
@@ -19,6 +31,16 @@ function sketchBox(canvas: HTMLCanvasElement, x: number, y: number, width: numbe
     context.restore();
 }
 
+/**
+ * Fills in a rectangle with dimensions `width` x `height` and upper left corner coordinate (x, y).
+ * 
+ * @param canvas 
+ * @param x upper left corner's x-coordinate
+ * @param y upper left corner's y-coordinate
+ * @param width 
+ * @param height 
+ * @param color 
+ */
 function fillBox(canvas: HTMLCanvasElement, x: number, y: number, width: number, height: number, color: string): void {
     const context = canvas.getContext("2d");
     if (context === undefined || context === null) {
@@ -30,43 +52,76 @@ function fillBox(canvas: HTMLCanvasElement, x: number, y: number, width: number,
     context.fillRect(0, 0, width, height);
     context.restore();
 }
-// TODO draw the grid first
-for (let i = 0; i < CANVAS_SIZE; i = i + NODE_SIZE) {
-    for (let j = 0; j < CANVAS_SIZE; j = j + NODE_SIZE) {
-        sketchBox(canvas, i, j, NODE_SIZE, NODE_SIZE, 1, 'black');
-    }
-}
 
+/**
+ * Converts flattened 2D matrix index to grid coordinates.
+ * 
+ * @param index flattened index location
+ * @returns coordinates where the origin is the upper left corner of the grid
+ */
 function indexToCoordinates(index: number): {x: number, y: number} {
     return {x: index % NODE_SIZE, y: Math.floor(index/NODE_SIZE)};
 }
 
+/**
+ * Converts pixel location to grid coordinates.
+ * 
+ * @param horizontal pixel's x-coordinate
+ * @param vertical pixel's y-coordinate
+ * @returns coordinates where the origin is the upper left corner of the grid
+ */
 function pixelToCoordinates(horizontal: number, vertical: number): {x: number, y: number} {
-    console.log(horizontal);
-    console.log(vertical);
     return {x: Math.floor(horizontal / NODE_SIZE), y: Math.floor(vertical/NODE_SIZE)};
 }
 
+/**
+ * Converts grid coordinates to flattened index.
+ * 
+ * @param x 
+ * @param y 
+ * @returns flattened index
+ */
 function coordinatesToIndex(x: number, y: number) {
     return x * NODE_SIZE + y;
 }
 
+/**
+ * Checks if the given index represents a corner grid block.
+ * 
+ * @param index flattened index
+ * @param size side length of square grid
+ * @returns true if the index represents a corner grid block.
+ */
 function isCornerIndex(index: number, size: number): boolean {
     return index === 0 || index === size - 1 || index === size*size - 1 || index === size * (size - 1);
 }
 
+/**
+ * Checks if the given index represents a side grid block.
+ * 
+ * @param index flattened index
+ * @param size side length of square grid
+ * @returns true if the index represents a side grid block.
+ */
 function isSideIndex(index: number, size: number): boolean {
     return Math.floor(index / size) === 0 || index % size === 0 || Math.floor(index / size) === size - 1 || index % size === size - 1;
 }
 
+/**
+ * Given a square grid of side length `size`, computes neighbors of all blocks in the grid.
+ * 
+ * @param size side length of square grid
+ * @returns adjacency list
+ */
 function generateSquareNeighbors(size: number): Map<number, Array<number>> {
     const neighbors = new Map<number, Array<number>>();
     const total = size * size;
     for (let i = 0; i < total; i++) {
         const currentNeighbors: Array<number> = [];
+
         // elements on the side of the square will not have all 4 possible neighbors.
         // possibilities: corner (2 neighbors), just the side (3 neighbors)
-        // do corners, happens when i === 0, i === size - 1, i === size * size - 1, i = size * (size - 1)
+        // corners
 
         if (isCornerIndex(i, size)) {
             switch (i) {
@@ -103,7 +158,7 @@ function generateSquareNeighbors(size: number): Map<number, Array<number>> {
             }
         }
 
-        // do sides
+        // sides
         else if (isSideIndex(i, size)) {
             // TODO
             if (Math.floor(i / size) === 0) { // top row
@@ -147,7 +202,6 @@ function generateSquareNeighbors(size: number): Map<number, Array<number>> {
             }
         }
 
-        // else
         else {
             const leftIndex = i - 1;
             const rightIndex = i + 1;
@@ -161,6 +215,14 @@ function generateSquareNeighbors(size: number): Map<number, Array<number>> {
     return neighbors;
 }
 
+/**
+ * Performs breadth-first search (BFS) on a graph beginning at the vertex `start`
+ * and finds the level sets.
+ * 
+ * @param start vertex to start BFS from
+ * @param adjacencies adjacency list
+ * @returns level sets indicating when vertices have been explored
+ */
 function BFS(start: number, adjacencies: Map<number, Array<number>>): Map<number, Array<number>> {
     // defensively copy
     const adjacencyList = new Map<number, Array<number>>();
@@ -216,26 +278,71 @@ function BFS(start: number, adjacencies: Map<number, Array<number>>): Map<number
     return levelsToNodes;
 }
 
+/**
+ * Returns a promise that will resolve after `ms` milliseconds.
+ * 
+ * @param ms number of milliseconds that the pause should last for
+ * @returns promise that resolves after `ms` milliseconds
+ */
 async function pause(ms: number): Promise<void> {
     return new Promise((resolve, reject) => {setTimeout(() => resolve(), ms);});
 }
-canvas.addEventListener('click', (async (event: MouseEvent) => {
-    console.log('here');
+
+/**
+ * Runs BFS and colors in the graph based on the user's choice of starting vertex.
+ * 
+ * @param event mouse click
+ */
+async function userBFS(event: MouseEvent, currentColor: Record<string, string>): Promise<void> {
     const coordinates = pixelToCoordinates(event.offsetY, event.offsetX);
     const index = coordinatesToIndex(coordinates.x, coordinates.y);
-    console.log(coordinates);
     const adjacencies = generateSquareNeighbors(NUMBER_NODES_PER_SIDE);
     const levelSets = BFS(index, adjacencies);
-    console.log(levelSets); // DEBUG OUTPUT, DELETE LATER
     const levels = levelSets.values();
     if (levels === undefined || levels === null) {throw new Error;}
+    currentColor = {r: '00', g: 'c1', b: 'ff'};
     for (const level of levels) {
         for (const vertex of level) {
             const coordinates = indexToCoordinates(vertex);
-            console.log(coordinates);
-            fillBox(canvas, coordinates.x*NODE_SIZE, coordinates.y*NODE_SIZE, NODE_SIZE, NODE_SIZE, 'black');
+            fillBox(canvas, coordinates.x*NODE_SIZE, coordinates.y*NODE_SIZE, NODE_SIZE, NODE_SIZE, `#${currentColor['r']}${currentColor['g']}${currentColor['b']}`);
         }
-        await pause(500);
+        const green = currentColor['g'];
+        if (green === undefined || green === null) {throw new Error;}
+        let currentColorValue = parseInt(green, 16);
+        currentColorValue += 3;
+        let stringifiedValue = currentColorValue.toString(16);
+        if (stringifiedValue.length > 2) {
+            stringifiedValue = "FF";
+        }
+        currentColor['g'] = stringifiedValue.length === 2? stringifiedValue : "0" + stringifiedValue;
+
+        const red = currentColor['r'];
+        if (red === undefined || red === null) {throw new Error;}
+        currentColorValue = parseInt(red, 16);
+        currentColorValue += 3;
+        stringifiedValue = currentColorValue.toString(16);
+        if (stringifiedValue.length > 2) {
+            stringifiedValue = "FF";
+        }
+        currentColor['r'] = stringifiedValue.length === 2? stringifiedValue : "0" + stringifiedValue;
+        await pause(100);
+
+    }
+}
+
+function main() {
+    // Draw the grid
+    for (let i = 0; i < CANVAS_SIZE; i = i + NODE_SIZE) {
+        for (let j = 0; j < CANVAS_SIZE; j = j + NODE_SIZE) {
+            sketchBox(canvas, i, j, NODE_SIZE, NODE_SIZE, 1, 'black');
+        }
     }
 
-}));
+    const currentColor = {'r': '00', 'g': 'c1', 'b': 'ff'}; // modify later to multiple starter gradients
+
+    canvas.addEventListener('click', (async (event: MouseEvent) => {
+        userBFS(event, currentColor);
+    }));
+}
+
+main();
